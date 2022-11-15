36c6828fa574090833878018e9a31b8b57410eb1
zaccarias <zaccariah23@gmail.com>
2022-11-15 10:47:08 +0800

    Initial commit

---
+++ Quiz.py
@@ -0,0 +1,8 @@
+name = input("Enter a UserName")
+age = input("Enter age")
+Numberings_1 = int(input("Enter First number: "))
+Numberings_2 = int(input("Enter Second number: "))
+
+X = str(Numberings_1 * Numberings_2)
+
+print("The username is" + name + "and the age is " + age + "\nThe product is " + X)
